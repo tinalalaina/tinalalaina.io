79564c8cc68baf0412c94f0a2d7b951248f3f8bf
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-09 00:05:01 -1200

    creation de page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-
-import './App.css';
-
+import React from 'react';
+import MaPage from './MaPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn <h2>Il est {new Date().toLocaleTimeString()}.</h2> React
-        </a>
-        
-      </header>
+    <div >
+      Mon application react
+      <MaPage/>
     </div>
   );
 }
---
+++ src/MaPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+const MaPage =()=>{
+    return(
+        <div>
+            <h1>Ma page</h1>
+        </div>
+    );
+};
+export default MaPage;
