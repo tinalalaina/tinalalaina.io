4d4253eb4f621a58b39385f0344bcc032620d02b
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-08 23:58:46 -1200

    initiation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,8 +14,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn <h2>Il est {new Date().toLocaleTimeString()}.</h2> React
         </a>
+        
       </header>
     </div>
   );
