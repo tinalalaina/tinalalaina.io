99d49b36ea688a8d14d8ec7be11de9344d8f2534
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-09 11:52:02 -1200

    ex1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import MaPage from './MaPage';
+import CV from './CV';
 function App() {
   return (
     <div >
       Mon application react
       <MaPage/>
+      <CV/>
+      
     </div>
   );
 }
---
+++ src/CV.js
@@ -0,0 +1,9 @@
+import React from "react";
+const CV =()=>{
+    return(
+        <div>
+            <h1>Mon Curriculum vitae</h1>
+        </div>
+    );
+};
+export default CV;
